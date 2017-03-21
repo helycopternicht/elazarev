a6fac0618afde3fe73831d91a53c0143f91ffcbf
Eugeniy Lazarev <helycopternicht@rambler.ru>
2017-03-21 19:52:23 +0600

    Add comments to Calcutale class and ru.job4j package

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j;
 
+/**
+* Class to calculate some math
+* @author Lazarev Eugene
+* @since 21/03/2017
+*/
 public class Calculate {
 	
+	/**
+	* Method for print Hello world string in console
+	* @author Lazarev Eugene
+	* @since 21/03/2017 
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello wold");
 	}

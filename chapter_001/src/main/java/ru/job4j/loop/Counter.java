87848cafef26be1af24d4fbd997f2e88c146d53a
Eugeniy Lazarev <helycopternicht@rambler.ru>
2017-03-23 19:39:21 +0600

    Fix formatting of method

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -16,11 +16,9 @@ public class Counter {
 	public int add(int start, int finish) {
 		int summ = 0;
 		for (int i = start; i <= finish; i++) {
-			if (i % 2 != 0) {
-				continue;
-			}
-
-			summ += i;
+			if (i % 2 == 0) {
+				summ += i;
+			}			
 		}
 		return summ;
 	}

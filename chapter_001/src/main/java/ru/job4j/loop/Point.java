e64aa66812fada2764a5a7d540c2ac71f374ff23
Eugeniy Lazarev <helycopternicht@rambler.ru>
2017-03-24 16:52:21 +0600

    Implemented class Point and test for it

---
+++ chapter_001/src/main/java/ru/job4j/loop/Point.java
@@ -0,0 +1,50 @@
+package ru.job4j.loop;
+
+/**
+* Class present point in 2d coodrinate system.
+* @author Eugene Lazarev mailto(helycopternicht@rambler.ru)
+* @since 24.03.2017
+*/
+public class Point {
+   /**
+   * Value on horisontal line.
+   */
+   private int x;
+   /**
+   * Value on vertical line.
+   */
+   private int y;
+  /**
+   * Default constructor.
+   * @param x - int Value on horisontal line
+   * @param y - int Value on vertical line
+   */
+   public  Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+  }
+  /**
+  * Getter for x value.
+  * @return value of x field
+  */
+  public int getX() {
+      return this.x;
+  }
+ /**
+  * Getter for y value.
+  * @return value of y field
+  */
+  public int getY() {
+     return this.y;
+  }
+
+  /**
+  * Method determine is point on the function or not.
+  * @param a integer
+  * @param b integer
+  * @return true is point on the function and false else
+  */
+  public boolean is(int a, int b) {
+    return this.y == a * this.x + b;
+  }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/PointTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Class for testing Point class.
+* @author Eugene Lazarev (mailto:helycopternicht@rambler.ru)
+* @version $Id$
+* @since 0.1
+*/
+public class PointTest {
+    /**
+    * Test is method.
+    */
+    @Test
+    public void whenXIsTwoAndYIsFourThenTrue() {
+        Point point = new Point(2, 4);
+        assertThat(point.is(1, 2), is(true));
+    }
+
+    /**
+    * Test is method.
+    */
+    @Test
+    public void whenXIsTwoAndYIsThreeThenFalse() {
+        Point point = new Point(2, 3);
+        assertThat(point.is(1, 2), is(false));
+    }
+}

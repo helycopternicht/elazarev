199ed523e7f5f1c0916e6051e716236c55260b40
Eugeniy Lazarev <helycopternicht@rambler.ru>
2018-02-04 12:28:43 +0600

    remove TestClass

--- chapter_007/toster/src/main/java/ru/elazarev/database/ConnectionFactory.java
+++ chapter_007/toster/src/main/java/ru/elazarev/database/ConnectionFactory.java
@@ -4,16 +4,27 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 /**
+ * Factory class for entity manager.
  * @author Eugene Lazarev mailto(helycopternicht@rambler.ru)
  * @since 30.01.18
  */
 public class EntityManagerF {
+    /**
+     * Factory store.
+     */
     private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("primary");
 
+    /**
+     * Returns entity manager factory.
+     * @return factory.
+     */
     public static EntityManagerFactory getFactory() {
         return factory;
     }
 
+    /**
+     * Private constructor.
+     */
     private EntityManagerF() {
     }
 }
